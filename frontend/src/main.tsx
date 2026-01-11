402142489cfaf39017237a6fec94b68a5d4b123b
Irsyaad Budi <irsyaadbp01@gmail.com>
2026-01-11 19:50:44 +0700

    feat: add sonner/toast component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import './main.css'
 import router from './router.tsx'
+import { Toaster } from './components/ui/sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
     <RouterProvider router={router} />
   </StrictMode>,
 )
