2c2dc51209606a3697b66c7c0a4af4ca20843bea
Irsyaad Budi <irsyaadbp01@gmail.com>
2026-01-11 22:53:58 +0700

    feat: update env name for mongo db

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async (): Promise<void> => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || '');
+        const conn = await mongoose.connect(process.env.MONGODB_URI || '');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${(error as Error).message}`);
