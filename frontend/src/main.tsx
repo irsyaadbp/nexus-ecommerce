5091887b36b905a9a0d486c5fa5534b6a61803ee
Irsyaad Budi <irsyaadbp01@gmail.com>
2026-01-11 19:52:26 +0700

    feat: add tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import './main.css'
 import router from './router.tsx'
-import { Toaster } from './components/ui/sonner'
+import { Toaster } from '@/components/ui/sonner'
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Toaster />
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
